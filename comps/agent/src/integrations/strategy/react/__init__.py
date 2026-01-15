82867abc612a92df250b0f81a7f1c27f8d818132
Haihao Xiang <haihao.xiang@intel.com>
2026-01-15 13:17:29 +0800

    Remove unnecessary files

--- comps/agent/src/integrations/strategy/react/__init__.py
+++
@@ -1,6 +0,0 @@
-# Copyright (C) 2024 Intel Corporation
-# SPDX-License-Identifier: Apache-2.0
-
-from .planner import ReActAgentwithLangchain
-from .planner import ReActAgentwithLanggraph
-from .planner import ReActAgentLlama
--- comps/arb_post_hearing_assistant/src/integrations/__init__.py
+++
@@ -1,2 +0,0 @@
-# Copyright (C) 2025 Zensar Technologies Private Ltd.
-# SPDX-License-Identifier: Apache-2.0
