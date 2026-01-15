f3852a267eadb30a186b2ec3b57be3723e470f6f
Haihao Xiang <haihao.xiang@intel.com>
2026-01-15 08:55:51 +0800

    Remove unnecessary files

--- comps/finetuning/src/integrations/xtune/adaclip_finetune/prepare_adaclip_finetune.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Copyright (C) 2025 Intel Corporation
-# SPDX-License-Identifier: Apache-2.0
-
-echo "prepare adaclip for xtune"
-git clone https://github.com/caoyunkang/AdaCLIP.git
-cd AdaCLIP && git fetch origin pull/45/head:xtune && git checkout xtune && cd .. && rsync -avPr AdaCLIP/  adaclip_finetune/ && rm -rf AdaCLIP
-echo "adaclip done"
--- comps/third_parties/mariadb/src/__init__.py
+++
@@ -1,2 +0,0 @@
-# Copyright (C) 2025 MariaDB Foundation
-# SPDX-License-Identifier: Apache-2.0
